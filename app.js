035ca3ffeedb1a96246b8472251f9743b9b739ea
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-04-18 22:50:10 +0800

    fix missing quote

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(function(){
     $.getJSON("file-map.json", function(data){
         data.forEach(function(image){
             var imgPath = "https://raw.githubusercontent.com/voz-living/voz-icon/master/repo/"+image;
-            $list.append("<div><img src='"+imgPath+"/><br/>" +
+            $list.append("<div><img src='"+imgPath+"'/><br/>" +
             "Image Path: <input type='text' class='copyable' value='"+imgPath+"'/>" +
             "<br/>BBCode: <input type='text'  class='copyable' value='[IMG]"+imgPath+"[/IMG]'/>" +
             "</div>");
