e07799b7018d19c7ae657b4c413b40178bd97140
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-04-18 23:31:22 +0800

    Update small size

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ $(function(){
             $list.append("<div><img src='"+imgPath+"'/><br/>" +
             "Image Path: <input type='text' class='copyable' value='"+imgPath+"'/>" +
             "<br/>BBCode: <input type='text'  class='copyable' value='[IMG]"+imgPath+"[/IMG]'/>" +
+            "<br/>BBCode (40px): <input type='text'  class='copyable' " +
+            "value='[IMG]https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url="+imgPath+"&container=focus&resize_w=40[/IMG]'/>" +
             "</div>");
         })
 
