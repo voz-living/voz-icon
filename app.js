8d1b255e4c783a7b14c49308d32b43c1d6e22512
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-04-18 22:48:48 +0800

    missing +

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(function(){
             var imgPath = "https://raw.githubusercontent.com/voz-living/voz-icon/master/repo/"+image;
             $list.append("<div><img src='"+imgPath+"/><br/>" +
             "Image Path: <input type='text' class='copyable' value='"+imgPath+"'/>" +
-            "<br/>BBCode: <input type='text'  class='copyable' value='[IMG]"+imgPath+"[/IMG]'/>"
+            "<br/>BBCode: <input type='text'  class='copyable' value='[IMG]"+imgPath+"[/IMG]'/>" +
             "</div>");
         })
 
