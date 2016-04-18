8f7ea98dbafb292117f6a45a98493f3cdb6ddef3
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-04-19 00:51:46 +0800

    Update mapping

--- build-map-file.js
+++ build-map-file.js
@@ -5,6 +5,7 @@ fs.readdir("../voz-icon-repo", function(err, files) {
     files.forEach(function(f) {
         if(/^(\.git|LICENSE)/.test(f)) return;
         fileList.push(f);
+        console.log("Adding: "+f);
     });
     fs.writeFileSync("file-map.json", JSON.stringify(fileList,null, 2));
     console.log("Done with written " + fileList.length + " files")
