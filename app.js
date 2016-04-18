570916e5eb2d36875ee4c7ce26e69d2eb523d916
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-04-18 22:47:56 +0800

    select path

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ $(function(){
     var $list = $("#icon-list");
     $.getJSON("file-map.json", function(data){
         data.forEach(function(image){
-            $list.append("<div><img src=''/>"+image+"</div>");
+            var imgPath = "https://raw.githubusercontent.com/voz-living/voz-icon/master/repo/"+image;
+            $list.append("<div><img src='"+imgPath+"/><br/>" +
+            "Image Path: <input type='text' class='copyable' value='"+imgPath+"'/>" +
+            "<br/>BBCode: <input type='text'  class='copyable' value='[IMG]"+imgPath+"[/IMG]'/>"
+            "</div>");
+        })
+
+        $(".copyable").on("click", function(){
+            $(this).select();
+            document.execCommand("copy")
         })
     })
 })
