4e46b58d353dd0ef33dc4f7632500e23771ea539
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-04-19 00:37:39 +0800

    move repo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(function(){
     var $list = $("#icon-list");
     $.getJSON("file-map.json", function(data){
         data.forEach(function(image){
-            var imgPath = "https://raw.githubusercontent.com/voz-living/voz-icon/master/repo/"+image;
+            var imgPath = "https://raw.githubusercontent.com/voz-living/voz-icon-repo/master/"+image;
             $list.append("<div><img src='"+imgPath+"'/><br/>" +
             "Image Path: <input type='text' class='copyable' value='"+imgPath+"'/>" +
             "<br/>BBCode: <input type='text'  class='copyable' value='[IMG]"+imgPath+"[/IMG]'/>" +
